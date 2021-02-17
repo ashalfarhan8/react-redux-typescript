325f90376a7bf5498cdaf380814245a86ef2f67b
ashalfarhan8 <ashalfarhan8@gmail.com>
2021-02-18 01:54:47 +0700

    add padding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import logo from "../logo.svg";
 
 const Header = () => {
   return (
-    <header className="App-header">
+    <header className="App-header py-4">
       <img src={logo} className="App-logo" alt="logo" />
       <p>Welcome to React, Redux, and Typescript</p>
     </header>
