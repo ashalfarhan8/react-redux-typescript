64b59298050174d137677fa4913eb1fd44596c24
ashalfarhan8 <ashalfarhan8@gmail.com>
2021-02-18 01:57:07 +0700

    create interface, import actions, and connect to react-redux

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,20 +1,24 @@
 import * as React from "react";
 import { connect } from "react-redux";
-import { fetchPosts } from "../actions/postAction";
+import { fetchPosts } from "../actions/postActions";
 import { Post } from "../types";
 
-const Posts = (props) => {
-  const [posts, setPosts] = React.useState<Post[]>([]);
+interface PostProps {
+  fetchPosts: () => void;
+  posts: Post[];
+}
+
+const Posts = ({ fetchPosts, posts }: PostProps) => {
+  // Invoked fetchPosts action when COMPONENT MOUNTED
   React.useEffect(() => {
-    props.fetchPosts();
-    console.log(props);
-  });
+    fetchPosts();
+  }, []);
   return (
     <div>
-      <h1 className="text-4xl text-center uppercase">Posts</h1>
-      {props.items.map((post, idx) => {
+      <h1 className="text-4xl text-center uppercase mb-12">Posts</h1>
+      {posts.map((post, idx) => {
         return (
-          <div>
+          <div key={idx} className="mx-4 mb-4">
             <h1 className="text-2xl font-semibold capitalize">{post.title}</h1>
             <p>{post.body}</p>
           </div>
@@ -24,8 +28,11 @@ const Posts = (props) => {
   );
 };
 
-const mapStateProps = (state) => ({
+// This will send a data to the props
+const mapStateProps = (state: any) => ({
   posts: state.posts.items,
 });
 
+// Inside curly braces is registering the
+// action to the props and we can use it inside the component
 export default connect(mapStateProps, { fetchPosts })(Posts);
