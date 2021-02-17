acc5e04af034bcb8bc2a577eb63eae8b3114d7d6
ashalfarhan8 <ashalfarhan8@gmail.com>
2021-02-18 01:55:46 +0700

    using action from postActions, add styling

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -1,15 +1,23 @@
 import React, { ChangeEvent, FormEvent } from "react";
-import { API_URL } from "../constants";
+import { connect } from "react-redux";
+import { createPost } from "../actions/postActions";
+import { SubmittedPost } from "../types";
 
-const PostForm = () => {
+interface PostFormProps {
+  createPost: (e: SubmittedPost) => void;
+}
+
+const PostForm = ({ createPost }: PostFormProps) => {
   const [title, setTitle] = React.useState<string>("");
   const [body, setBody] = React.useState<string>("");
   const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
   };
+
   const handleBodyChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setBody(e.target.value);
   };
+
   const handleSubmitForm = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
@@ -17,39 +25,51 @@ const PostForm = () => {
       title,
       body,
     };
-    fetch(API_URL, {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(post),
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data));
+
+    // Call Action
+    // From where that action received ?
+    // see below
+    createPost(post);
+    // Dont forget to clear the state
     setTitle("");
     setBody("");
   };
   return (
-    <div>
-      <h1 className="text-4xl text-center uppercase">Add Posts</h1>
-      <form className="max-w-sm w-full mx-auto" onSubmit={handleSubmitForm}>
-        <div className="flex flex-col">
-          <label>Title: </label>
+    <div className="mb-24 mt-4">
+      <h1 className="text-4xl text-center mb-4">Add a post</h1>
+      <form
+        className="max-w-sm w-full mx-auto flex flex-col bg-gray-700 py-4 rounded-xl"
+        onSubmit={handleSubmitForm}
+      >
+        <div className="flex flex-col mx-4">
+          <label className="text-white">Title: </label>
           <input
             type="text"
             name="title"
             onChange={handleTitleChange}
             value={title}
+            className="p-2 outline-none focus:outline-none"
           />
         </div>
-        <div className="flex flex-col">
-          <label>Body: </label>
-          <textarea name="body" value={body} onChange={handleBodyChange} />
+        <div className="flex flex-col mx-4 mb-4">
+          <label className="text-white">Body: </label>
+          <textarea
+            name="body"
+            value={body}
+            onChange={handleBodyChange}
+            className="p-2 outline-none focus:outline-none"
+          />
         </div>
-        <input type="submit" value="Submit" />
+        <input
+          type="submit"
+          value="Submit"
+          className="py-2.5 mx-4 outline-none focus:outline-none cursor-pointer"
+        />
       </form>
     </div>
   );
 };
 
-export default PostForm;
+// Registering the actions in the connect method
+// Then it will be passed to the props of this component
+export default connect(null, { createPost })(PostForm);
