5d2157267f645567c3fe10aa51c8a2c388ae071f
ashalfarhan8 <ashalfarhan8@gmail.com>
2021-02-18 01:53:25 +0700

    replace note app with post app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-// import { addNote } from "./actions";
-// import NewNote from "./components/NewNote";
-// import { NoteState } from "./store/notesReducer";
-// import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import React from "react";
 import Header from "./components/Header";
@@ -9,26 +5,11 @@ import Posts from "./components/Posts";
 import PostForm from "./components/PostForm";
 
 function App() {
-  // const notes = useSelector<NoteState, NoteState["notes"]>(
-  //   (state) => state.notes
-  // );
-  // const dispatch = useDispatch();
-  // const handleAddNote = (note: string) => {
-  //   dispatch(addNote(note));
-  // };
-
   return (
     <div>
       <Header />
       <PostForm />
       <Posts />
-      {/* <NewNote addNote={handleAddNote} />
-      <hr />
-      <ul>
-        {notes.map((note, idx) => {
-          return <li key={idx}>{note}</li>;
-        })}
-      </ul> */}
     </div>
   );
 }
